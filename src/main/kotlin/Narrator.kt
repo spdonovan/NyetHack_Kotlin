8f48ed425cd70cebab296cebf304c6cfe11d8cd0
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-07 14:18:44 +0000

    First Commit

---
+++ src/main/kotlin/Narrator.kt
@@ -0,0 +1,9 @@
+fun narrate(
+    message: String
+) {
+    val narrationModifier: () -> String = {
+        val numExclamationPoints = 3
+        message.uppercase() + "!".repeat(numExclamationPoints)
+    }
+    println(narrationModifier())
+}
---
+++ src/main/kotlin/NyetHack.kt
@@ -0,0 +1,4 @@
+fun main() {
+    narrate("A hero enters the town of Kronstadt. What is their name?")
+    val heroName = readln() ?: ""
+}
