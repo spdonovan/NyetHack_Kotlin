5efc6bf0767b20601034c912c0b0ab7849613596
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-07 16:04:02 +0000

    Narrator moods using when function

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,11 +1,44 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-    val narrationModifier: (String) -> String = {
-        val numExclamationPoints = 3
-        it.uppercase() + "!".repeat(numExclamationPoints)
-    }
+var narrationModifier: (String) -> String = { it }
 
     fun narrate(
         message: String
     ) {
     println(narrationModifier(message))
 }
+    fun ChangeNarratorMood() {
+        val mood: String
+        val modifier: (String) -> String
+
+        when (Random.nextInt(1..4)) {
+            1 -> {
+                mood = "loud"
+                modifier = { message ->
+                    val numExclamationPoints = 3
+                    message.uppercase() + "!".repeat(numExclamationPoints)
+                }
+            }
+            2 -> {
+                mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+            }
+            3 -> {
+                mood = "unsure"
+                modifier = { message ->
+                    "$message?"
+                }
+            }
+            else -> {
+                mood = "professional"
+                modifier = { message ->
+                    "$message."
+                }
+            }
+        }
+        narrationModifier = modifier
+        narrate("The narrator begins to feel $mood")
+    }
