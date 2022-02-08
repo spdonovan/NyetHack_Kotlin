96e405b540d4d560c04f5ca0ae34f074b97d5eec
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-08 11:34:58 +0000

    tavern menu data list file created

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,5 +1,10 @@
+import java.io.File
+
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
+private val menuData = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
 
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
@@ -20,9 +25,17 @@ fun visitTavern() {
     }
     println(othersMessage)
 
-    narrate("Eli leaves the tavern")
-    patrons.remove("Eli")
-    narrate("Alex enters the tavern")
-    patrons.add("Alex")
-    println(patrons)
+        patrons.forEachIndexed { index, patron ->
+        println("Good evening, $patron - you're #${index + 1} in line")
+            placeOrder(patron, "Dragon's Breath")
+    }
+
+    menuData.forEachIndexed { index, data ->
+        println("$index : $data")
+    }
+}
+
+private fun placeOrder(patronName: String, menuItem: String) {
+    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
+    narrate("$TAVERN_MASTER hands $patronName a $menuItem")
 }
