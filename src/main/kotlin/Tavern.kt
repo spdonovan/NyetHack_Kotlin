448557fd1433e4dcc2ec6fbc75d9769f43178018
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-08 10:50:26 +0000

    mutable list of patrons added to tavern

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -2,5 +2,27 @@ private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
 fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
 
+    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
+
+    val eliMessage = if (patrons.contains("Eli")) {
+        "$TAVERN_MASTER says: Eli's in the back playing cards"
+    } else {
+        "$TAVERN_MASTER says: Eli isn't here"
+    }
+    println(eliMessage)
+
+    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
+        "$TAVERN_MASTER says: Sophie and Mordoc are seated at the kettle"
+    } else {
+        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    }
+    println(othersMessage)
+
+    narrate("Eli leaves the tavern")
+    patrons.remove("Eli")
+    narrate("Alex enters the tavern")
+    patrons.add("Alex")
+    println(patrons)
 }
