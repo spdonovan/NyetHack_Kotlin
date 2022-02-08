5fb7310608b0450bfb80aa5d368bfe59c26f5d2e
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-08 12:08:15 +0000

    mutable sets of patrons and random generated names

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -2,40 +2,36 @@ import java.io.File
 
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
+
+private val firstNames = setOf("Alex", "Mordoc", "Sophie", "tariq")
+private val lastNames = setOf("Ironfoot", "Fernswerth", "Baggins", "Downstrider")
+
 private val menuData = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
 
+private val menuItems = List(menuData.size) { index ->
+val (_, name, _) = menuData[index].split(",")
+name
+}
+
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
+    narrate("There are several items for sale:")
+    narrate(menuItems.joinToString())
 
-    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
-
-    val eliMessage = if (patrons.contains("Eli")) {
-        "$TAVERN_MASTER says: Eli's in the back playing cards"
-    } else {
-        "$TAVERN_MASTER says: Eli isn't here"
+    val patrons: MutableSet<String> = mutableSetOf()
+    repeat(10) {
+        patrons += "${firstNames.random()} ${lastNames.random()}"
     }
-    println(eliMessage)
+    narrate("$heroName sees several patrons in the tavern:")
+    narrate(patrons.joinToString())
 
-    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
-        "$TAVERN_MASTER says: Sophie and Mordoc are seated at the kettle"
-    } else {
-        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
-    }
-    println(othersMessage)
-
-        patrons.forEachIndexed { index, patron ->
-        println("Good evening, $patron - you're #${index + 1} in line")
-            placeOrder(patron, "Dragon's Breath")
-    }
-
-    menuData.forEachIndexed { index, data ->
-        println("$index : $data")
+    repeat(3) {
+        placeOrder(patrons.random(), menuItems.random())
     }
 }
 
 private fun placeOrder(patronName: String, menuItem: String) {
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
-    narrate("$TAVERN_MASTER hands $patronName a $menuItem")
-}
+    narrate("$TAVERN_MASTER hands $patronName a $menuItem")}
